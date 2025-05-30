6fe315d8cdf1787eab1f8632c9a90db059d5c905
gmbcode <gmbhacademic@gmail.com>
2025-05-30 17:27:13 +0530

    Feat : Added category and priority db that does not sync

--- User_Tasks.py
+++ User_Tasks.py
@@ -4,16 +4,18 @@ from User import User
 from typing import Any
 import os
 from dotenv import dotenv_values
+
 config = dotenv_values(".env")
 
-def load_local_db(nosync = False) -> dict[str, Any] | None:
+
+def load_local_db(nosync=False) -> dict[str, Any] | None:
     """Load local database and return local db dict"""
     if not nosync:
         db_selected = config['USER_TASKS_DB']
     else:
         db_selected = config['USER_NOSYNC_DB']
     if not os.path.exists(db_selected):
-        try :
+        try:
             with open(db_selected, "w") as file:
                 file.write("{}")
         except OSError:
@@ -26,53 +28,77 @@ def load_local_db(nosync = False) -> dict[str, Any] | None:
     except (json.JSONDecodeError, FileNotFoundError):
         return None
 
-def save_local_db(db) -> bool | None:
+
+def save_local_db(db, nosync=False) -> bool | None:
     """Save changed local db"""
-    if not os.path.exists(config['USER_TASKS_DB']):
-        try :
-            with open(config['USER_TASKS_DB'], "w") as file:
+    if not nosync:
+        db_selected = config['USER_TASKS_DB']
+    else:
+        db_selected = config['USER_NOSYNC_DB']
+    if not os.path.exists(db_selected):
+        try:
+            with open(db_selected, "w") as file:
                 file.write("{}")
         except OSError:
             return None
 
     try:
-        with open(config['USER_TASKS_DB'], 'w') as f:
+        with open(db_selected, 'w') as f:
             json.dump(db, f, indent=2, default=str)
         return True
     except (json.JSONDecodeError, FileNotFoundError):
         return None
 
 
-def sync_tasks_c2l(userid : str) -> None:
+def sync_tasks_c2l(userid: str) -> None:
     """Sync cloud changes to local database"""
     t_user = User(userid)
     if t_user.user_exists():
-          l_db = load_local_db()
-          if userid not in l_db:
-              l_db[userid] = {}
-          t_client = GoogleTasksClient(userid)
-          tasklists = []
-          tasks = []
-          task_lists = t_client.get_task_lists()
-          for task_list in task_lists:
-              tasklists.append(task_list)
-              tasks.append( t_client.get_tasks(task_list['id'])['items'])
-          l_db[userid]['tasklists'] = tasklists
-          l_db[userid]['tasks'] = tasks
-          save_local_db(l_db)
+        l_db = load_local_db()
+        l_db_ns = load_local_db(nosync=True)
+        u_set = True
+        if userid not in l_db:
+            l_db[userid] = {}
+        if userid not in l_db_ns:
+            l_db_ns[userid] = {}
+            u_set = False
+            l_db_ns[userid]["categories"] = []
+
+        t_client = GoogleTasksClient(userid)
+        tasklists = []
+        tasks = []
+        tasks_ns = []
+        task_lists = t_client.get_task_lists()
+        for task_list in task_lists:
+            tasklists.append(task_list)
+            res = t_client.get_tasks(task_list['id'])['items']
+            for item in res:
+                tasks.append(item)
+        if not u_set:
+            for st in tasks:
+                tasks_ns.append({"id": st["id"], "title": st["title"], "status": st["status"], "priority": "not_set",
+                                 "category": "not_set"})
+            l_db_ns[userid]["tasks"] = tasks_ns
+            save_local_db(l_db_ns, nosync=True)
+        l_db[userid]['tasklists'] = tasklists
+        l_db[userid]['tasks'] = tasks
+        save_local_db(l_db)
 
     else:
         raise Exception(f"User {userid} does not exist")
 
-def create_task_synced(userid : str,task : dict[str,Any],task_list_id : str) -> None:
+
+def create_task_synced(userid: str, task: dict[str, Any], task_list_id: str) -> None:
     """Create a task and sync immediately to local db"""
     t_user = User(userid)
     if t_user.user_exists():
         client = GoogleTasksClient(userid)
-        client.create_task(task_list_id=task_list_id,title=task["title"],notes=task["notes"],due=task["due"])
+        client.create_task(task_list_id=task_list_id, title=task["title"], notes=task["notes"], due=task["due"])
         sync_tasks_c2l(userid)
     else:
         raise Exception(f"User {userid} does not exist")
+
+
 if __name__ == '__main__':
     sync_tasks_c2l("585767830247571476")
     pass
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from discord.ext import commands
 from dotenv import dotenv_values
 from Auth_Server import generate_auth_url
 from User import User as TskUser
-from datetime import datetime,date,timedelta
+from datetime import datetime, date, timedelta
 from Tasks import GoogleTasksClient
 from table2ascii import table2ascii
-from User_Tasks import sync_tasks_c2l
+from User_Tasks import sync_tasks_c2l, load_local_db, save_local_db
 from Misc_Methods import str_to_task
+
 config = dotenv_values(".env")
 TOKEN_DISCORD = config["DISCORD_BOT_TOKEN"]
 format_str = "Task Name Due Date(YYYY-MM-DD) Notes(if any)\nExample : my task 2025-05-30 Some details about my task"
@@ -16,15 +17,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="#", intents=intents)
-def status_converter(status : str) -> str:
+
+
+def status_converter(status: str) -> str:
     """Miniature task status converter"""
     if status == "completed":
         return "✓"
     else:
         return "X"
+
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
 
 @bot.event
 async def on_message(message):
@@ -38,7 +44,7 @@ async def on_message(message):
             else:
                 await message.channel.send(f'Initialising\nYour member id is {message.author.id}')
                 await message.channel.send('Click on the link and authorize within 60 seconds to continue')
-                await message.channel.send(generate_auth_url(message.author.id),delete_after=60.0)
+                await message.channel.send(generate_auth_url(message.author.id), delete_after=60.0)
                 for i in range(6):
                     if us.user_exists():
                         await message.channel.send(f"You are successfully initialised")
@@ -49,21 +55,26 @@ async def on_message(message):
                     await asyncio.sleep(10)
                 await  message.channel.send("Authorisation cancelled")
 
+
 @bot.command()
 async def list_tasks(ctx):
     """Function to list all the users tasks , the tasklist and their due time"""
     if ctx.channel.type == discord.ChannelType.private:
         us = TskUser(str(ctx.author.id))
         if us.user_exists():
-            await ctx.channel.send(f"Loading task list for user {ctx.author.name} (Synced from Google Tasks)",delete_after=10)
+            await ctx.channel.send(f"Loading task list for user {ctx.author.name} (Synced from Google Tasks)",
+                                   delete_after=10)
             try:
                 clt = GoogleTasksClient(user_id=str(ctx.author.id))
                 tl = clt.get_task_lists()
             except Exception as e:
                 await ctx.channel.send("Error fetching user tasks")
                 return
-            headings = ['Task Name','Due Date','Tasklist','Status']
+            headings = ['Category','Task Name', 'Due Date', 'Tasklist', 'Status','Priority']
             resp = []
+            sync_tasks_c2l(str(ctx.author.id))
+            ns_db = load_local_db(nosync = True)
+            ns_db = ns_db[str(ctx.author.id)]['tasks']
             for task_list in tl:
                 tr = clt.get_tasks(task_list['id'])['items']
 
@@ -73,18 +84,20 @@ async def list_tasks(ctx):
                         f_date = f_date.strftime("%B %d, %Y")
                     else:
                         f_date = 'Not Set / Passed'
-                    resp.append([task['title'], f_date,task_list['title'],status_converter(task['status'])])
+                    current_task = list(filter(lambda t: t['id'] == task['id'], ns_db))[0]
+                    resp.append([current_task['category'],task['title'], f_date, task_list['title'], status_converter(task['status']), current_task['priority']])
             if len(resp) == 0:
                 await ctx.channel.send("No tasks found")
                 return
-            final_response = table2ascii(header=headings,body=resp)
-            final_response = '```\n'+ final_response + '\n```'
-            sync_tasks_c2l(str(ctx.author.id))
-            chunks = [final_response[i:i + 2000] for i in range(0, len(final_response), 2000)] # Split into 2000 sized chunks
+            final_response = table2ascii(header=headings, body=resp)
+            final_response = '```\n' + final_response + '\n```'
+            chunks = [final_response[i:i + 2000] for i in
+                      range(0, len(final_response), 2000)]  # Split into 2000 sized chunks
             for chunk in chunks:
                 await ctx.channel.send(chunk)
         else:
             await ctx.channel.send(f"Initialise by typing initialise first and register yourself")
+
 
 @bot.command()
 async def list_tasklists(ctx):
@@ -94,18 +107,19 @@ async def list_tasklists(ctx):
         if us.user_exists():
             await ctx.channel.send(f"Loading task list for user {ctx.author.name} (Synced from Google Tasks)",
                                    delete_after=10)
-            try :
+            try:
                 clt = GoogleTasksClient(user_id=str(ctx.author.id))
                 tl = clt.get_task_lists()
             except Exception as e:
                 await ctx.channel.send("Error fetching user tasklists")
                 return
-            headings = ['Tasklist Name','Last Updated']
+            headings = ['Tasklist Name', 'Last Updated']
             resp = []
             for task_list in tl:
-                resp.append([task_list['title'], datetime.fromisoformat(task_list['updated']).astimezone().strftime("%B %d, %Y")])
+                resp.append([task_list['title'],
+                             datetime.fromisoformat(task_list['updated']).astimezone().strftime("%B %d, %Y")])
             final_response = table2ascii(header=headings, body=resp)
-            final_response = '```\n'+ final_response + '\n```'
+            final_response = '```\n' + final_response + '\n```'
             sync_tasks_c2l(str(ctx.author.id))
             chunks = [final_response[i:i + 2000] for i in
                       range(0, len(final_response), 2000)]  # Split into 2000 sized chunks
@@ -114,14 +128,16 @@ async def list_tasklists(ctx):
         else:
             await ctx.channel.send(f"Initialise by typing initialise first and register yourself")
 
+
 @bot.command()
 async def create_tasklist(ctx):
     """Function to create a new tasklist"""
     if ctx.channel.type == discord.ChannelType.private:
         us = TskUser(str(ctx.author.id))
         if us.user_exists():
-            await ctx.channel.send(f"Enter tasklist name to create below",delete_after=20)
-            def validator(message : discord.Message) -> bool:
+            await ctx.channel.send(f"Enter tasklist name to create below", delete_after=20)
+
+            def validator(message: discord.Message) -> bool:
                 if len(message.content) == 0 or len(message.content) > 100:
                     return False
                 return True
@@ -131,25 +147,27 @@ async def create_tasklist(ctx):
                 clt = GoogleTasksClient(user_id=str(ctx.author.id))
                 clt.create_task_list(message.content)
                 await ctx.channel.send(f"Tasklist {message.content} created successfully")
-                await ctx.channel.send(f"Current Tasklists ",delete_after=21)
+                await ctx.channel.send(f"Current Tasklists ", delete_after=21)
                 tl = clt.get_task_lists()
-                headings = ['Tasklist Name','Last Updated']
+                headings = ['Tasklist Name', 'Last Updated']
                 resp = []
                 for task_list in tl:
-                    resp.append([task_list['title'], datetime.fromisoformat(task_list['updated']).astimezone().strftime("%B %d, %Y")])
+                    resp.append([task_list['title'],
+                                 datetime.fromisoformat(task_list['updated']).astimezone().strftime("%B %d, %Y")])
                 final_response = table2ascii(header=headings, body=resp)
-                final_response = '```\n'+ final_response + '\n```'
+                final_response = '```\n' + final_response + '\n```'
                 sync_tasks_c2l(str(ctx.author.id))
                 chunks = [final_response[i:i + 2000] for i in
                           range(0, len(final_response), 2000)]  # Split into 2000 sized chunks
                 for chunk in chunks:
-                    await ctx.channel.send(chunk,delete_after=20)
+                    await ctx.channel.send(chunk, delete_after=20)
             except asyncio.TimeoutError:
                 await ctx.channel.send("Time elapsed please try again later")
             except Exception as e:
                 await ctx.channel.send("Error fetching user ")
         else:
             await ctx.channel.send(f"Initialise by typing initialise first and register yourself")
+
 
 @bot.command()
 async def create_task(ctx):
@@ -158,16 +176,16 @@ async def create_task(ctx):
         us = TskUser(str(ctx.author.id))
         if us.user_exists():
             try:
-                await ctx.channel.send(f"Enter tasklist number to add the task to : ",delete_after=10)
+                await ctx.channel.send(f"Enter tasklist number to add the task to : ", delete_after=10)
                 clt = GoogleTasksClient(user_id=str(ctx.author.id))
                 tl = clt.get_task_lists()
-                headings = ['No','Tasklist Name','Last Updated']
+                headings = ['No', 'Tasklist Name', 'Last Updated']
                 resp = []
                 id_list = []
                 index = 0
                 for task_list in tl:
                     id_list.append(task_list['id'])
-                    resp.append([str(index),task_list['title'],
+                    resp.append([str(index), task_list['title'],
                                  datetime.fromisoformat(task_list['updated']).astimezone().strftime("%B %d, %Y")])
                     index += 1
                 final_response = table2ascii(header=headings, body=resp)
@@ -177,55 +195,62 @@ async def create_task(ctx):
                           range(0, len(final_response), 2000)]  # Split into 2000 sized chunks
                 for chunk in chunks:
                     await ctx.channel.send(chunk, delete_after=20)
-                def validator(message : discord.Message) -> bool:
-                    try :
+
+                def validator(message: discord.Message) -> bool:
+                    try:
                         ct = int(message.content)
-                        if ct < 0 or (ct > (len(resp) - 1) ) :
+                        if ct < 0 or (ct > (len(resp) - 1)):
                             return False
                         return True
                     except ValueError:
                         return False
-                def task_validator(message : discord.Message) -> bool:
+
+                def task_validator(message: discord.Message) -> bool:
                     tokenized = message.content.split()
                     index = 0
                     d_index = 0
                     d_toks = 0
                     for token in tokenized:
-                        try :
-                            datetime.strptime(token,"%Y-%m-%d")
+                        try:
+                            datetime.strptime(token, "%Y-%m-%d")
                             d_toks += 1
                             d_index = index
-                        except ValueError :
+                        except ValueError:
                             pass
                         index += 1
-                    if d_toks == 1: # Check if only one date is present in input
+                    if d_toks == 1:  # Check if only one date is present in input
                         pass
                     else:
                         return False
-                    if d_index != 0 :
+                    if d_index != 0:
                         task_name = tokenized[0:d_index]
                         task_name = "".join(task_name)
                     else:
                         return False
-                    task_due = datetime.strptime(tokenized[d_index],"%Y-%m-%d").date()
+                    task_due = datetime.strptime(tokenized[d_index], "%Y-%m-%d").date()
                     today = date.today()
-                    if task_due < today: # Date is in the past
+                    if task_due < today:  # Date is in the past
                         return False
                     year = timedelta(days=365)
                     max_spread = today + year
-                    if task_due > max_spread: # Task is more than one year after
+                    if task_due > max_spread:  # Task is more than one year after
                         return False
                     return True
 
-
                 message = await bot.wait_for('message', timeout=10.0, check=validator)
                 tl_no = int(message.content)
-                await ctx.channel.send(f'You have selected tasklist {resp[tl_no][1]}',delete_after=20)
-                await ctx.channel.send(f"Enter Details in the following format : \n{format_str}",delete_after=21)
+                await ctx.channel.send(f'You have selected tasklist {resp[tl_no][1]}', delete_after=20)
+                await ctx.channel.send(f"Enter Details in the following format : \n{format_str}", delete_after=21)
                 task_msg = await bot.wait_for('message', timeout=30.0, check=task_validator)
                 task = str_to_task(task_msg.content)
                 print(task)
-                clt.create_task(task_list_id=id_list[tl_no],title=task[0],notes=task[1],due=task[2])
+                new_task = clt.create_task(task_list_id=id_list[tl_no], title=task[0], notes=task[1], due=task[2])
+                sync_tasks_c2l(str(ctx.author.id))
+                ns_db = load_local_db(nosync=True)
+                ns_db[str(ctx.author.id)]["tasks"].append(
+                    {"id": new_task["id"], "title": new_task["title"], "status": new_task["status"],
+                     "priority": "not_set", "category": "not_set"})
+                save_local_db(ns_db, nosync=True)
                 await ctx.channel.send(f"Task {task[0]} created successfully in tasklist {resp[tl_no][1]}")
 
             except asyncio.TimeoutError:
@@ -235,24 +260,6 @@ async def create_task(ctx):
                 await ctx.channel.send("Error fetching user / Task Creation Error ")
         else:
             await ctx.channel.send(f"Initialise by typing initialise first and register yourself")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 bot.run(TOKEN_DISCORD)
